c557a97bb5d8329751cbf94adaca93e4a7130bd0
Marius Orcsik <marius@federated.id>
2021-09-21 21:30:23 +0200

    Reimplementing some bits of ListenAndServeX
    This helps with uniformizing the way we instantiate listeners between http, https and socket

--- http_server.go
+++ http_server.go
@@ -3,6 +3,8 @@ package wrapper
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -18,80 +20,126 @@ func fileExists(dir string) bool {
 type (
 	c struct {
 		h        http.Handler
+		l        net.Listener
 		wTimeOut time.Duration
 		cert     string
 		key      string
 		addr     string
 	}
-	SetFn func(*c)
+	SetFn func(*c) error
 )
 
 func WriteWait(d time.Duration) SetFn {
-	return func(c *c) {
+	return func(c *c) error {
 		c.wTimeOut = d
+		return nil
 	}
 }
 
-func ListenOn(addr string) SetFn {
-	return func(c *c) {
+func HTTP(addr string) SetFn {
+	return func(c *c) (err error) {
+		if addr == "" {
+			addr = ":http"
+		}
 		c.addr = addr
+		c.l, err = net.Listen("tcp", c.addr)
+		return
 	}
 }
 
-func SSL(cert, key string) SetFn {
-	if !fileExists(cert) || !fileExists(key) {
-		return func(*c) {}
+func HTTPS(addr, cert, key string) SetFn {
+	if !fileExists(cert) {
+		return func(*c) error { return fmt.Errorf("invalid certificate file %q", cert) }
 	}
-	return func(c *c) {
-		c.cert = cert
-		c.key = key
+	if !fileExists(key){
+		return func(*c) error { return fmt.Errorf("invalid key file %q", key) }
+	}
+	return func(c *c) (err error) {
+		if addr == "" {
+			addr = ":https"
+		}
+		c.l, err = net.Listen("tcp", addr)
+		return
 	}
 }
 
 func Handler(h http.Handler) SetFn {
-	return func(c *c) {
+	return func(c *c) error {
 		c.h = h
+		return nil
 	}
 }
+
+func Socket() SetFn {
+	return func (c *c) (err error) {
+		c.l, err = net.FileListener(os.NewFile(3, "from systemd"))
+		return
+	}
+}
+
+var (
+	defaultTLSConfig = tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	defaultRunFn = func() error {
+		return nil
+	}
+)
 
 // HttpServer initializes a http.Server object with values set using SetFn() functions
 func HttpServer(ctx context.Context, setters ...SetFn) (func() error, func() error) {
 	c := new(c)
+	var serveFn, stopFn func() error
 	for _, fn := range setters {
-		fn(c)
+		if err := fn(c); err != nil {
+			serveFn = func() error { return err }
+		}
+		stopFn = defaultRunFn
 	}
-	var serveFn func() error
+	if c.l == nil {
+		serveFn = func() error { return fmt.Errorf("no listeners have been configured") }
+	}
 
 	srv := &http.Server{
 		Handler:      c.h,
 		Addr:         c.addr,
 		WriteTimeout: c.wTimeOut,
 	}
-	if fileExists(c.key) && fileExists(c.cert) {
-		srv.TLSConfig = &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
-		}
+	switch c.l.(type) {
+	case *net.UnixListener:
 		serveFn = func() error {
-			return srv.ListenAndServeTLS(c.cert, c.key)
+			return srv.Serve(c.l)
 		}
-	} else {
-		serveFn = func() error {
-			return srv.ListenAndServe()
+	case *net.TCPListener:
+		if len(c.cert) > 0 && len(c.key) > 0{
+			serveFn = func() error {
+				return srv.ServeTLS(c.l, c.cert, c.key)
+			}
+		} else {
+			serveFn = func() error {
+				return srv.Serve(c.l)
+			}
 		}
+	}
+	stopFn = func() error {
+		return c.l.Close()
 	}
 
 	stop := func() error {
 		if err := srv.Shutdown(ctx); err != nil {
+			return err
+		}
+		if err := stopFn(); err != nil {
 			return err
 		}
 		select {
