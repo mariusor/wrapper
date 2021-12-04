3033a1099e0f967f8947499129dc5bd8503afd81
Marius Orcsik <marius@habarnam.ro>
2021-12-04 20:58:04 +0100

    Allowing multiple listeners

--- http_server.go
+++ http_server.go
@@ -21,14 +21,17 @@ func fileExists(dir string) bool {
 type (
 	c struct {
 		h        http.Handler
-		l        net.Listener
+		l        []net.Listener
+		s        []http.Server
 		wTimeOut time.Duration
 		cert     string
 		key      string
-		addr     string
+		errFn    func(s string, p ...interface{})
 	}
 	SetFn func(*c) error
 )
+
+func nilErrFn(_ string, _ ...interface{}) {}
 
 func WriteWait(d time.Duration) SetFn {
 	return func(c *c) error {
@@ -38,13 +41,16 @@ func WriteWait(d time.Duration) SetFn {
 }
 
 func HTTP(addr string) SetFn {
-	return func(c *c) (err error) {
+	return func(c *c) error {
 		if addr == "" {
 			addr = ":http"
 		}
-		c.addr = addr
-		c.l, err = net.Listen("tcp", c.addr)
-		return
+		l, err := net.Listen("tcp", addr)
+		if err != nil {
+			return err
+		}
+		c.l = append(c.l, l)
+		return nil
 	}
 }
 
@@ -55,14 +61,18 @@ func HTTPS(addr, cert, key string) SetFn {
 	if !fileExists(key) {
 		return func(*c) error { return fmt.Errorf("invalid key file %q", key) }
 	}
-	return func(c *c) (err error) {
+	return func(c *c) error {
 		if addr == "" {
 			addr = ":https"
 		}
 		c.key = key
 		c.cert = cert
-		c.l, err = net.Listen("tcp", addr)
-		return
+		l, err := net.Listen("tcp", addr)
+		if err != nil {
+			return err
+		}
+		c.l = append(c.l, l)
+		return nil
 	}
 }
 
@@ -74,20 +84,37 @@ func Handler(h http.Handler) SetFn {
 }
 
 func Socket(s string) SetFn {
-	return func(c *c) (err error) {
-		c.l, err = net.Listen("unix", s)
-		return
+	return func(c *c) error {
+		l, err := net.Listen("unix", s)
+		if err != nil {
+			return err
+		}
+		c.l = append(c.l, l)
+		return nil
 	}
 }
 
 func Systemd() SetFn {
-	return func(c *c) (err error) {
-		nfds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
-		if err != nil || nfds == 0 {
+	nfds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
+	if err != nil || nfds == 0 {
+		return func(_ *c) error {
 			return fmt.Errorf("it appears that we're not expected to wait for a systemd socket connection")
 		}
-		c.l, err = net.FileListener(os.NewFile(3, "Systemd listen fd"))
-		return
+	}
+	return func(c *c) error {
+		l, err := net.FileListener(os.NewFile(3, "Systemd listen fd"))
+		if err != nil {
+			return err
+		}
+		c.l = append(c.l, l)
+		return nil
+	}
+}
+
+func Err(errFn func(s string, p ...interface{})) SetFn {
+	return func(c *c) error {
+		c.errFn = errFn
+		return nil
 	}
 }
 
@@ -115,12 +142,64 @@ var (
 	}
 )
 
+func (c *c) start() error {
+	c.s = make([]http.Server, 0, len(c.l))
+	errChan := make(chan error, len(c.l))
+	for _, l := range c.l {
+		srv := http.Server{
+			Handler:      c.h,
+			Addr:         l.Addr().String(),
+			WriteTimeout: c.wTimeOut,
+		}
+		c.s = append(c.s, srv)
+		switch l.(type) {
+		case *net.UnixListener:
+			go func() {
+				errChan <- srv.Serve(l)
+			}()
+		case *net.TCPListener:
+			if len(c.cert) > 0 && len(c.key) > 0 {
+				go func() {
+					errChan <- srv.ServeTLS(l, c.cert, c.key)
+				}()
+			} else {
+				go func() {
+					errChan <- srv.Serve(l)
+				}()
+			}
+		}
+	}
+	select {
+	case err := <-errChan:
+		return err
+	}
+	return nil
+}
+
+func (c *c) stop(ctx context.Context) error {
+	var err error
+	for i, l := range c.l {
+		l.Close()
+		srv := c.s[i]
+		if err := srv.Shutdown(ctx); err != nil {
+			return err
+		}
+	}
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+	return err
+}
+
 // HttpServer initializes a http.Server object with values set using SetFn() functions
 func HttpServer(setters ...SetFn) (func() error, func(context.Context) error) {
-	c := new(c)
-	var startFn func() error
+	c := c{
+		l:     make([]net.Listener, 0),
+		errFn: nilErrFn,
+	}
 	for _, fn := range setters {
-		if err := fn(c); err != nil {
+		if err := fn(&c); err != nil {
 			return errStartFn(err), emptyStopFn
 		}
 	}
@@ -128,39 +207,5 @@ func HttpServer(setters ...SetFn) (func() error, func(context.Context) error) {
 		return errStartFn(fmt.Errorf("no listeners have been configured")), emptyStopFn
 	}
 
-	srv := http.Server{
-		Handler:      c.h,
-		Addr:         c.addr,
-		WriteTimeout: c.wTimeOut,
-	}
-	stopFn := func(ctx context.Context) error {
-		if err := c.l.Close(); err != nil {
-			return err
-		}
-		if err := srv.Shutdown(ctx); err != nil {
-			return err
-		}
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		}
-	}
-	switch c.l.(type) {
-	case *net.UnixListener:
-		startFn = func() error {
-			return srv.Serve(c.l)
-		}
-	case *net.TCPListener:
-		if len(c.cert) > 0 && len(c.key) > 0 {
-			startFn = func() error {
-				return srv.ServeTLS(c.l, c.cert, c.key)
-			}
-		} else {
-			startFn = func() error {
-				return srv.Serve(c.l)
-			}
-		}
-	}
-
-	return startFn, stopFn
+	return c.start, c.stop
 }
