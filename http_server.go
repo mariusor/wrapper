186252a019344978deda73c47bc49711683f2574
Marius Orcsik <marius@federated.id>
2022-10-08 14:10:56 +0200

    Remove deprecated property

--- http_server.go
+++ http_server.go
@@ -120,9 +120,8 @@ func Err(errFn func(s string, p ...interface{})) SetFn {
 
 var (
 	defaultTLSConfig = tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
+		MinVersion:       tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
